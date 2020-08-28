e84b9292f2ae1f190622efe28db823b95d752d0b
ErikHansenGit <70379148+ErikHansenGit@users.noreply.github.com>
2020-08-28 14:59:07 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+ Erik Hansen, 26.08.2020
+ The following codes compute the pressure equilibrium when a flat rigid plane is loaded against an ideal elastic-plastic profile. The profile deformation is computed with the elastic half-space model. Subsurface plastic deformation is neglected, only plastic flow on the surface is taken into account. Outside of the contact zone, a constant hydrodynamic pressure can be prescribed. This pressure also serves as a lower limit for the pressure in the contact zone.
+ The codes denoted with akchurin compute the pressure profile based on an imposed rigid body seperation h_s.
+ The codes denoted with polonsky compute the pressure profile based on an imposed normal load force W_aim.
+ The algorithm is based on the conjugate gardient method and fast Fourier transformations. 
+ The codes denoted with linear use a linear convolution to determine the elastic deformation due to the pressure profile. Use this e.g. for single concentrated contacts.
+ The codes denoted with circular use a circular convolution to determine the elastic deformation due to the pressure profile. This corresponds to periodic boundary conditions. Use this e.g. for periodic roughness profiles.
---
+++ elpl_contact_pressure_polonsky_circular_script.m
@@ -0,0 +1,376 @@
+close all; clc; clear all;
+
+%% Settings
+% Algorithm settings:
+W_aim   = 10;                                                               % [N] imposed normal load force
+err_tol = 1e-10;                                                            % [-] relative error tolerance
+h_ref   = 1e-6;                                                             % [m] reference length of relative error
+it_max  = 1e2;                                                              % [-] maximum number of iterations
+
+% Fluid settings:
+p_hd = 1e5;                                                                 % [Pa] fluid pressure in non-contact zone
+
+% Solid settings:
+sld.nu_low      = 0.3;                                                      % [-] Poisson ratio of lower body
+sld.E_low       = 210e9;                                                    % [Pa] Young's modulus of lower body
+sld.nu_up       = 0.208;                                                    % [-] Poisson ratio of upper body
+sld.E_up        = 81e9;                                                     % [Pa] Young's modulus of upper body
+sld.E_dash      = 2/((1 - sld.nu_low^2)/sld.E_low + (1 - sld.nu_up^2)/sld.E_up); % [Pa] reduced modulus of elasticity
+sld.H           = 5e8;                                                      % [Pa] hardness of the softer material as a maximum limit of the contact pressure
+
+% Geometry settings: 
+geo.Nx1         = 2^7;                                                      % [-] number of discretization points in the x1-direction
+geo.Nx2         = 2^7;                                                      % [-] number of discretization points in the x2-direction
+x1_Wb           = -0.25e-3;                                                 % [m] x1-coordinate of cell at the West boundary
+x1_Eb           = 0.25e-3;                                                  % [m] x1-coordinate of cell at the East boundary
+x2_Sb           = -0.25e-3;                                                 % [m] x2-coordinate of cell at the South boundary
+x2_Nb           = 0.25e-3;                                                  % [m] x2-coordinate of cell at the North boundary
+geo.Rx1         = 1.27e-2;                                                  % [m] ball radius
+
+% Construct geometry:
+geo.dx1         = (x1_Eb - x1_Wb)/(geo.Nx1 - 1);                            % [m] Spacing in x1-direction
+geo.dx2         = (x2_Nb - x2_Sb)/(geo.Nx2 - 1);                            % [m] Spacing in x2-direction
+geo.x1          = linspace(x1_Wb,x1_Eb,geo.Nx1);                            % [m] x1-coordinates with uniform discretization
+geo.x2          = linspace(x2_Sb,x2_Nb,geo.Nx2);                            % [m] x2-coordinates with uniform discretization
+clear x1_Wb, clear x1_Eb; clear x2_Sb; clear x2_Nb;
+
+% Add sinosoidal roughness:
+a           = 1e-6;                                                         % [m] roughness amplitude
+lambda_x1   = (geo.x1(geo.Nx1) - geo.x1(1) + geo.dx1)/3;                              % [m] roughness wavelengh in x1-direction
+lambda_x2   = (geo.x2(geo.Nx2) - geo.x2(1) + geo.dx2)/0.5;                  % [m] roughness wavelengh in x2-direction
+[x1, x2]    = ndgrid(geo.x1,geo.x2); % Convert vectors to matrizes
+rough_prof  = a*sin(x1/lambda_x1*2*pi).*cos(x2/lambda_x2*2*pi);             % [m] roughness profile
+clear x1; clear x2; clear lambda_x1; clear lambda_x2; clear a;
+% Superimpose roughness on profile:
+geo.prof    = rough_prof;        
+clear rough_prof;
+
+%% Computation
+% Compute 2-D fast Fourier transform of the elastic half-space Kernel
+[fft2_Kernel_circ]  = construct_circular_Kernel(geo.Nx1,geo.Nx2,geo.dx1,geo.dx2,sld.E_dash);
+
+% Check limits for normal load:  
+W_min = geo.Nx1*geo.dx1*geo.Nx2*geo.dx2*p_hd;
+W_max = geo.Nx1*geo.dx1*geo.Nx2*geo.dx2*sld.H;
+if W_aim <= W_min
+    fprintf('Warning!\n')
+    fprintf('W_aim <= W_min = %d\n',W_min)
+    fprintf('h_s is not unambigous since there is no surface contact!\n')
+elseif W_aim >= W_max
+    fprintf('Warning!\n')
+    fprintf('W_aim >= W_max = %d\n',W_max)
+    fprintf('h_s is not unambigous since there is plastic deformation in each grid point!\n')
+end
+
+% Compute pressure field:  
+[p_con,g,err] = elpl_contact_pressure_polonsky_circular(p_hd,sld.H,geo.prof,W_aim,geo.Nx1,geo.Nx2,geo.dx1,geo.dx2,fft2_Kernel_circ,err_tol,it_max,h_ref);
+% Compute resulting normal load:
+W = geo.dx1*geo.dx2*sum(p_con(:));
+
+% Compute deformations and deformed profiles:
+[h_el] = compute_h_el_circular(p_con,geo.Nx1,geo.Nx2,fft2_Kernel_circ);                   % [m] elastic deformation
+clear fft2_Kernel;
+z_el = geo.prof - h_el;                                                     % [m] elastically deformed profile
+A_el = find(p_con>p_hd & p_con<sld.H);                                      % [-] indices of elastically deformed points
+if  isempty(A_el)
+    fprintf('Warning!\n')
+    fprintf('h_s is not unambigous since there is plastic deformation in each contact grid point!\n')
+end
+h_s = mean(z_el(A_el));                                                     % [m] seperating distance between upper and lower reference planes
+clear A_el;
+A_pl = find(p_con>=sld.H);                                                  % [-] indices of plastically deformed points
+h_pl = zeros(geo.Nx1,geo.Nx2);
+h_pl(A_pl) = z_el(A_pl) - h_s;                                              % [m] plastic deformation                          
+clear A_pl;
+z_elpl = z_el - h_pl;                                                       % [m] elastically and plastically deformed profile
+
+%% Plot
+% Plot seetings:
+% Line plots:
+KIT_colorlist={[0,150,130]/255,[162 34 35]/255,[70 100 170]/255,[252 229 0]/255,[140 182 60]/256,[223 155 27]/255,[167 130 46]/255,[163 16 124]/255,[35 161 224]/255};
+widthlines          = 1.2;
+% Surface plots:
+colmap = parula;
+
+% Plots:
+figure('Units','centimeters','Position',[01 13 7 7])
+surf(geo.x1,geo.x2,geo.prof')
+xlabel('x_1 [m]')
+ylabel('x_2 [m]')
+zlabel('z [m]')
+title('Undeformed profile')
+shading interp
+material dull
+colormap(colmap)
+camlight
+
+figure('Units','centimeters','Position',[09 13 7 7])
+surf(geo.x1,geo.x2,h_el')
+xlabel('x_1 [m]')
+ylabel('x_2 [m]')
+zlabel('h_{el} [m]')
+title('Elastic deformation')
+shading interp
+material dull
+colormap(colmap)
+camlight
+
+figure('Units','centimeters','Position',[17 13 7 7])
+surf(geo.x1,geo.x2,h_pl')
+xlabel('x_1 [m]')
+ylabel('x_2 [m]')
+zlabel('h_{pl} [m]')
+title('Plastic deformation')
+shading interp
+material dull
+colormap(colmap)
+camlight
+
+figure('Units','centimeters','Position',[25 13 7 7])
+surf(geo.x1,geo.x2,z_elpl')
+xlabel('x_1 [m]')
+ylabel('x_2 [m]')
+zlabel('h_{pl} [m]')
+title('Deformed profile')
+shading interp
+material dull
+colormap(colmap)
+camlight
+
+figure('Units','centimeters','Position',[01 00 7 7])
+semilogy(err,'LineWidth',widthlines,'Color',KIT_colorlist{1})
+xlabel('i_{it} [m]')
+ylabel('err [-]')
+title('Relative error')
+
+figure('Units','centimeters','Position',[11 00 7 7])
+surf(geo.x1,geo.x2,p_con')
+xlabel('x_1 [m]')
+ylabel('x_2 [m]')
+zlabel('p_{con} [m]')
+title('Contact pressure')
+shading interp
+material dull
+colormap(colmap)
+camlight
+
+figure('Units','centimeters','Position',[21 00 7 7])
+surf(geo.x1,geo.x2,g')
+xlabel('x_1 [m]')
+ylabel('x_2 [m]')
+zlabel('g [m]')
+title({'Residual'; '{\itAim in hydrodynamic region: g<0}'; '{\itAim in elastic region: g=0}'; '{\itAim in plastic region: g>0}'})
+shading interp
+material dull
+colormap(colmap)
+camlight
+
+clear KIT_colorlist; clear colmap; clear widthlines;
+
+function [p_con,g,err] = elpl_contact_pressure_polonsky_circular(p_min,H,z,W_aim,Nx1,Nx2,dx1,dx2,fft2_Kernel_circ,err_tol,it_max,h_ref)
+% Erik Hansen, 26.08.2020
+% Calculates the contact pressure occuring when a rigid smooth surface is
+% loaded against an elastic half-space with ideal elastic-plastic material
+% behaviour. The code is very similar to and consists largely of the 
+% algorithm described Polonsky and Keer, 1999:
+% "A numerical method for solving rough contact problems based on the multi-level multi-summation and conjugate gradient techniques"
+% and Akchurin et al., 2015:
+% "On a model for the prediction of the friction coefficient in mixed lubrication based on a load-sharing concept with measured surface roughness"
+% However, some changes were introduced in the treatment of non-contact and
+% plastic contact points. Inspiration was taken from Vollebregt, 2014:
+% "A new solver for the elastic normal contact problem using conjugate gradients, deflation, and an FFT-based preconditioner"
+% The Matlab implementation of the linear convolution in the fourier space
+% was largely inspired by Sainsot and Lubrecht, 2011:
+% "Efficient solution of the dry contact of rough surfaces: a comparison of fast Fourier transform and multigrid methods"
+% Input:
+% p_min             [Pa]    pressure in non contact zones as a lower limit for the contact pressure
+% H                 [Pa]    hardness of the softer material as a maximum limit of the contact pressure
+% z                 [m]     undeformed profile
+% W_aim             [N]     imposed normal load force
+% Nx1               [-]     number of points in x1-direction
+% Nx2               [-]     number pf points in x2-direction
+% dx1               [m]     cell length in x1-direction
+% dx2               [m]     cell length in x2-direction
+% fft2_Kernel       [m/Pa]  2-D fast Fourier transform of the Kernel
+% err_tol           [-]     relative error tolerance
+% it_max            [-]     maximum number of iterations
+% h_ref             [m]     reference length of relative error
+% Output:
+% p_con             [Pa]    contact pressure field
+% g                 [m]     residual of the gap height distribution
+% err               [-]     relative error
+% -------------------------------------------------------------------------
+G_ref  = h_ref^2;                               % [m^2] reference norm of the residual
+p_con = W_aim/(Nx1*dx1*Nx2*dx2)*ones(Nx1,Nx2);  % [Pa] pressure field
+[u] = compute_h_el_circular(p_con,Nx1,Nx2,fft2_Kernel_circ);  % [m] elastic deformation 
+g = -u + z;                                     % [m] residual of the gap height distribution
+
+% Find indices of points that are in the elastic domain due to the pressure
+% distribution:
+A_el    = find(p_con>p_min  &p_con<H);
+
+% Set residual reference plane to zero in the elastic domain:
+g_mean  = mean(g(A_el));
+g       = g - g_mean;
+clear g_mean;
+
+% Find indices of points that are in the non-contact and plastic
+% domain due to the pressure distribution. Non-contact and plastic points 
+% are also evaluated whether they are correctly or not in surface contact
+% due to the gap height distribution
+A_nc_cr = find(p_con<=p_min &g<0);
+A_nc_wr = find(p_con<=p_min &g>=0);
+A_pl_cr = find(p_con>=H     &g>=0);
+A_pl_wr = find(p_con>=H     &g<0);
+% Within these points, the pressure distribution needs to be adjusted:
+A_free  = union(union(A_el,A_nc_wr),A_pl_wr);
+ 
+G       = sum(g(A_free).*g(A_free));    % [m^2] norm of the residual
+G_old   = 1;                            % [m^2] previous norm of the residual
+delta   = 0;                            % [-] flag whether to use conjugate gradient or steepest descend
+err     = zeros(it_max,1);              % [-] relative error
+i_it    = 0;                            % [-] iteration counter        
+t       = zeros(Nx1,Nx2);               % [Pa] search direction
+while i_it == 0 || (err(i_it)>err_tol && i_it<=it_max)
+    i_it = i_it + 1;    
+    
+    % Find search direction:
+    t(A_free)   = g(A_free) + delta*(G/G_old)*t(A_free);
+    t(A_nc_cr)  = 0;
+    t(A_pl_cr)  = 0;
+    clear delta;
+    
+    % Determine step length tau:
+    [t_conv]    = compute_h_el_circular(t,Nx1,Nx2,fft2_Kernel_circ);     
+    r           = -t_conv;
+    clear t_conv;   
+    % Adjust reference plane:
+    r_mean  = mean(r(A_el));
+    r       = r - r_mean;
+    clear r_mean;
+    % Compute tau:
+    tau = (sum(g(A_free).*t(A_free)))/(sum(r(A_free).*t(A_free)));
+    clear r;
+
+    % Update pressure and confine it to the upper and lower limit:
+    p_con(A_free)       = p_con(A_free) - tau*t(A_free);
+    p_con(p_con<p_min)  = p_min;
+    p_con(p_con>H)      = H;
+    
+    % Rescale the pressure in the free domain in such a way that the 
+    % desired normal load W_aim is met by the whole pressure profile:
+    A_tot               = linspace(1,Nx1*Nx2,Nx1*Nx2)';
+    A_unscalable        = setdiff(A_tot,A_free);
+    W_scalable          = sum(p_con(A_free))*dx1*dx2;
+    W_unscalable        = sum(p_con(A_unscalable))*dx1*dx2;
+    p_con(A_free)       = (W_aim - W_unscalable)/W_scalable*p_con(A_free);
+    clear A_tot; clear A_unscalable; clear W_scalable; clear W_unscalable;
+    
+    % Find indices of points that are in the elastic domain due to the pressure
+    % distribution:
+    A_el    = find(p_con>p_min  &p_con<H);
+    
+    % Compute elsatic deformation to find new residual of the gap height distribution
+    [u] = compute_h_el_circular(p_con,Nx1,Nx2,fft2_Kernel_circ); 
+    g = -u + z;
+    clear u;
+    
+    % Set residual reference plane to zero in the elastic domain:
+    g_mean  = mean(g(A_el));
+    g       = g - g_mean;
+    clear g_mean;
+    
+    % Find indices of points that are in the non-contact and plastic
+    % domain due to the pressure distribution. Non-contact and plastic points 
+    % are also evaluated whether they are correctly or not in surface contact
+    % due to the gap height distribution
+    A_nc_cr = find(p_con<=p_min &g<0);
+    A_nc_wr = find(p_con<=p_min &g>=0);
+    A_pl_cr = find(p_con>=H     &g>=0);
+    A_pl_wr = find(p_con>=H     &g<0);
+    % Within these points, the pressure distribution needs to be adjusted:
+    A_free = union(union(A_el,A_nc_wr),A_pl_wr);
+    
+    % Determine whether to use conjugate gradient or steepest descend in the next ieration
+    if isempty(A_nc_wr) && isempty(A_pl_wr)
+    	delta = 1;
+    else
+        delta = 0;
+    end
+
+    % Save G for the next iteration:
+    G_old = G;
+    % Compute norm of the residual:
+    G = sum(g(A_free).*g(A_free));
+    % Compute relative error
+    err(i_it) = abs(G - G_old)/G_ref;
+end
+% Resize err:
+err = err(1:i_it,1);
+end
+
+
+function [h_el] = compute_h_el_circular(p,Nx1,Nx2,fft2_Kernel_circ)
+% Computes elastic displacement due to pressure field with circular
+% convolution in Fourier space
+% Input:
+% p                 [Pa]        pressure field
+% Nx1               [-]         number of discretized points in x1-direction
+% Nx2               [-]       	number of discretized points in x2-direction
+% fft2_Kernel_circ  [m/Pa]      2-D fast Fourier transform of the Kernel
+% Output:
+% h_el              [m]         elastic deformation
+% -------------------------------------------------------------------------
+% Compute convolution in Fourier space for better pefromance:
+h_el            = ifft2(fft2_Kernel_circ.*fft2(p));
+end
+
+function [fft2_Kernel_circ]  = construct_circular_Kernel(Nx1,Nx2,dx1,dx2,E_dash)
+% Calculates the Kernel function for a circular convolution in the
+% influence area of size Nx1*Nx2
+% The Kernel is constructed for an imposed normal load as explained by Pohrt and Li, 2014:
+% "Complete boundary element formulation for normal and tangential contact problems"
+% under the assumption of isotropic material behaviour and therefore 
+% sld.E = 2*(sld.nu + 1)*sld.G
+% and the definition of 
+% sld.E_dash = 2/((1 - sld.nu_low^2)/sld.E_low + (1 - sld.nu_up^2)/sld.E_up).
+% The Kernel center is at the edge of the domain
+% Input:
+% Nx1                 [-]       Number of discretized points in x1-direction
+% Nx2                 [-]       Number of discretized points in x2-direction
+% dx1                 [m]       Length of discretized cell in x1-direction
+% dx2                 [m]       Length of discretized cell in x2-direction
+% E_dash              [Pa]      reduced modulus of elasticity
+% Output:
+% fft2_Kernel_circ    [m/Pa]    2-D fast Fourier transform of the Kernel
+% -------------------------------------------------------------------------
+Nx1_K = Nx1;      % [-]       Number of discretized Kernel points in x1-direction
+Nx2_K = Nx2;      % [-]       Number of discretized Kernel points in x2-direction
+dx1_mod = dx1/2;
+dx2_mod = dx2/2;
+% Determine distances:
+i           = 1:Nx1_K;
+j           = 1:Nx2_K;
+i_cond      = (i <= floor(Nx1_K/2) + 1);
+x1          = (((floor(Nx1_K/2) + 1) - (i - (ceil(Nx1_K/2) + 1))) - 1)*dx1;
+x1(i_cond)  = (i(i_cond) - 1)*dx1;
+j_cond      = (j <= floor(Nx2_K/2) + 1);
+x2          = (((floor(Nx2_K/2) + 1) - (j - (ceil(Nx2_K/2) + 1))) - 1)*dx2;
+x2(j_cond)  = (j(j_cond) - 1)*dx2;
+clear i; clear j; clear i_cond; clear j_cond;
+clear Nx1; clear Nx2; clear dx1; clear dx2;
+[x1, x2]    = ndgrid(x1,x2); % Convert vectors to matrizes
+% Construct Kernel:
+term_1 = (x1 + dx1_mod).*log(ext_sqrt(x2 + dx2_mod, x1 + dx1_mod)./ext_sqrt(x2 - dx2_mod, x1 + dx1_mod));
+term_2 = (x2 + dx2_mod).*log(ext_sqrt(x1 + dx1_mod, x2 + dx2_mod)./ext_sqrt(x1 - dx1_mod, x2 + dx2_mod));
+term_3 = (x1 - dx1_mod).*log(ext_sqrt(x2 - dx2_mod, x1 - dx1_mod)./ext_sqrt(x2 + dx2_mod, x1 - dx1_mod));
+term_4 = (x2 - dx2_mod).*log(ext_sqrt(x1 - dx1_mod, x2 - dx2_mod)./ext_sqrt(x1 + dx1_mod, x2 - dx2_mod));
+clear x1;  clear x2; clear dx1_mod;  clear dx2_mod; 
+Kernel        = 2/(pi*E_dash)*(term_1 + term_2 + term_3 + term_4);
+clear term_1; clear term_2; clear term_3; clear term_4;
+fft2_Kernel_circ    = fft2(Kernel);
+function return_value = ext_sqrt(p,q)
+% Auxiliary function for Kernel construction
+return_value = p + sqrt(p.^2 + q.^2);
+end
+end
